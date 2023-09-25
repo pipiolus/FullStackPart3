37e1f092c61c0baac463c69392f922479d726182
pipiolus <giovanipaolonii@gmail.com>
2023-09-25 14:33:45 -0300

    App ready to be deployed

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 morgan.token("content", (req) => JSON.stringify(req.body));
