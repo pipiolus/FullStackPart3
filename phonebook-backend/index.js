1ba6f023b2188998ab639119cbf826eec8bbc41e
pipiolus <giovanipaolonii@gmail.com>
2023-09-25 10:56:25 -0300

    Added Morgan (phonebook backend step7 and step8)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+morgan.token("content", (req) => JSON.stringify(req.body));
+
 app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] :response-time ms :content"
+  )
+);
 
 let persons = [
   {
