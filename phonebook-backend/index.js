e91466c0271ad959923770b348337e316da6ef6e
pipiolus <giovanipaolonii@gmail.com>
2023-09-25 14:45:17 -0300

    New build to redeploy (Changes on dist folder)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.static("dist"));
+app.use(cors());
 app.use(express.json());
 
 morgan.token("content", (req) => JSON.stringify(req.body));
